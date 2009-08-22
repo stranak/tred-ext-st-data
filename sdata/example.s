af5ca077c028249092980c5ae0fc393bc364ab69
Pavel Stranak <pavel.stranak@gmail.com>
2009-08-22 20:25:15 +0200

    Archive of creation of s=layer PML scheme. Some files are probably obsolete.

---
+++ sdata/example.s
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<sdata xmlns="http://ufal.mff.cuni.cz/pdt/pml/">
+ <head>
+  <schema href="sdata_schema.xml" />
+  <references>
+   <reffile id="m" name="mdata" href="example.m" />
+   <reffile id="t" name="tdata" href="example.t" />
+   <reffile id="s" name="semlex" href="/path/to/semlex.xml" />
+  <!-- TODO: For the lexicon used <lexicon> in <s-annotation-info> should be
+          referenced -->
+  </references>
+ </head>
+ <meta>
+  <annotation_info>
+      <lexicon>SemLex 1.0</lexicon>
+      <annotator>PaSt</annotator>
+      <version_info>first shot</version_info>
+      <desc>Manual annotation</desc>
+  </annotation_info>
+ </meta>
+ <wsd>
+     <st id="12345">
+         <lexicon-id>s#0000066468</lexicon-id>
+         <t.rf>t#m-example-p2s1w1</t.rf>
+     </st>
+ </wsd>
+</sdata>
