b03b3669f058268e56f0aab144fdcae423f7d2d4
Pavel Stranak <pavel.stranak@gmail.com>
2009-09-17 21:55:12 +0200

    More merging done, but not finished.

--- twig-merge.pl
+++ twig-merge.pl
@@ -27,34 +27,35 @@ $sfile =~ /.st$/ || die "This is not an 'st' file.";
 
 my $stwig = new XML::Twig;
 $stwig->parsefile("$sfile");
-my $sroot  = $stwig->root;
-my @sdata = $root->children;
+my $sroot = $stwig->root;
+my @sdata = $sroot->children;
 my @st    = $sdata[2]->children;
 for my $snode (@st) {
     my @t_refs = $snode->children('t.rf');
     for my $trf (@t_refs) {
-       print $trf->text, "\n"; 
+        print $trf->text, "\n";
     }
     print "\n";
 }
 
-my $tfile = substr($sfile, 0, -1);
+my $tfile = substr( $sfile, 0, -1 );
 my $ttwig = new XML::Twig;
 $ttwig->parsefile("$tfile");
-my $troot  = $ttwig->root;
+my $troot = $ttwig->root;
 my @tdata = $troot->children;
 my @ttree = $tdata[2]->children;
-foreach my $snode (@sdata){
+foreach my $snode (@sdata) {
     my @trf = $snode->children('t.rf');
     my $s_first_id = $trf[0] =~ s/t#t/t/;
-    foreach my $troot (@ttrees) {
-        if match($troot, $snode){
+    foreach my $troot (@ttree) {
+        if (match( $troot, $snode )) {
             $snode->paste($troot);
-        }
+        };
     }
 }
 
 sub match {
+
     # projit strom (asi rekurzi) a overit, jestli obsahuje children/LM/@id =
     # $s_first_id
 }
