a50f427e350d2c43ac88087c4b9d7622ce1d5e4a
Pavel Stranak <pavel.stranak@gmail.com>
2009-09-21 14:39:43 +0200

    Moved and renamed the merging script. Now it uses the new tdata_mwe PML schema.

--- tred-integration/merge-s-and-t-layer.pl
+++ tred-integration/merge-s-and-t-layer.pl
@@ -1,21 +1,21 @@
 #!/usr/bin/perl 
 #===============================================================================
 #
-#         FILE:  twig-merge.pl
+#         FILE:  merge-s-and-t-layer.pl
 #
-#        USAGE:  ./twig-merge.pl
+#        USAGE:  ./merge-s-and-t-layer.pl <st-file>
 #
 #  DESCRIPTION:  Integrate the s-layer annotation into the t-layer files.
 #
 #      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  Will create duplicate <st> nodes, if there are some on input. 
+# REQUIREMENTS:  S-files and t-files uncompressed, a,m,w files gzipped.
+#         BUGS:  Will create duplicate <st> nodes, if there are already some
+#                in the input t-file.
 #        NOTES:  ---
 #       AUTHOR:  Pavel Stranak (stranak@ufal.mff.cuni.cz),
 #      COMPANY:
 #      VERSION:  1.0
 #      CREATED:  2009/09/16 15:14:58
-#     REVISION:  ---
 #===============================================================================
 
 use strict;
@@ -35,9 +35,11 @@ my @st    = $sdata[2]->children;
 # Parse t-file and get t-trees
 my $tfile = substr( $sfile, 0, -2 );
 $tfile = $tfile . 't';
-my $ttwig = new XML::Twig(pretty_print => 'nice');
+my $ttwig = new XML::Twig( pretty_print => 'nice' );
 $ttwig->parsefile("$tfile");
 my $troot = $ttwig->root;
+my ($sch) = $troot->get_xpath('/tdata/head/schema');
+$sch->set_att('href', 'tdata_mwe_schema.xml');
 my @tdata = $troot->children;
 my @ttree = $tdata[2]->children;
 
@@ -57,24 +59,12 @@ SNODE: foreach my $snode (@st) {
     } @tnodes;
     my $s_first_tnode = $tnodes[0]->text;
 
-    #########################################################
-    # TODO change this: get rid of 't#' in place (or a copy),
-    #    my @trf_node = $snode->children('t.rf');
-    #    my @trf_text;
-    #    foreach my $node (@trf_node) {
-    #        my $t = $node->text;
-    #        $t =~ s/t#t/t/;
-    #        push @trf_text, $t;
-    #    }
-    #    my $s_first_id = $trf_text[0];
-    ####### end of a block to modify #######################
-
   TNODE: foreach my $troot (@ttree) {
         no warnings;
         my @lmembers  = $troot->descendants('LM');
         my @tnode_ids = map { $_->att('id') } @lmembers;
         my $match     = grep { $_ eq $s_first_tnode } @tnode_ids;
-        my ($mwes_exist, $mwes);
+        my ( $mwes_exist, $mwes );
         if ( $troot->first_child('mwes') ) {
             $mwes_exist = 1;
             $mwes       = $troot->first_child('mwes');
@@ -85,10 +75,8 @@ SNODE: foreach my $snode (@st) {
         if ($match) {
             $mwes->paste($troot) if not $mwes_exist;
             $snode->move( 'last_child', $mwes );
-
-            #            $troot->print('indented');
-            #            die;
         }
     }
 }
-$ttwig->print();
+open( my $out, '>', "$ARGV[0]" . ".mwe" );
+$ttwig->print($out);
