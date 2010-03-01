bfe348d0486f71261f47f8850046008a94f12d71
Pavel Stranak <pavel.stranak@gmail.com>
2010-03-01 12:40:59 +0100

    Promitnuti uprav Petra Pajase. Upravy maji za cil umoznit merge on-the-fly pri otevreni s-souboru tredem. Signed-off-by: Pavel Stranak <pavel.stranak@gmail.com>

--- tred-integration/merge-s-and-t-layer.pl
+++ tred-integration/merge-s-and-t-layer.pl
@@ -3,7 +3,7 @@
 #
 #         FILE:  merge-s-and-t-layer.pl
 #
-#        USAGE:  ./merge-s-and-t-layer.pl <st-files>
+#        USAGE:  ./merge-s-and-t-layer.pl [--stdout|-S] <st-files>
 #
 #  DESCRIPTION:  Integrate the s-layer annotation into the t-layer files.
 #
@@ -19,71 +19,32 @@ use strict;
 use warnings;
 use XML::LibXML;
 
+use FindBin qw($Bin);
+use lib "$FindBin::Bin";
+use SDataMerge;
+use Getopt::Long;
+GetOptions("stdout|S" => \ our $use_stdout)
+  or die "Usage: $0 [--stdout|-S] <st-files>\n";
+
 my $st_suffix = qr/\.st\.g?zi?p?$/;
 my $t_suffix  = qr/\.t\.g?zi?p?$/;
 
 foreach my $s_filename (@ARGV) {
 
     # Parse s-file and get s-nodes
-    if ( not $s_filename =~ $st_suffix ) {
+    if ($s_filename !~ $st_suffix ) {
         warn "$s_filename is not an 'st' file.";
         next;
     }
     my $parser = XML::LibXML->new();
     $parser->keep_blanks(0);
     my $sdoc   = $parser->parse_file($s_filename);
-    my $s_cont = XML::LibXML::XPathContext->new( $sdoc->documentElement() );
-    $s_cont->registerNs( pml => 'http://ufal.mff.cuni.cz/pdt/pml/' );
-
-    # Parse t-file and get t-trees
-    $s_filename =~ s/$st_suffix//;
-    my ($t_filename) = <$s_filename.t*>;
-    my $tdoc = $parser->parse_file($t_filename);
-    my $t_cont = XML::LibXML::XPathContext->new( $tdoc->documentElement() );
-    $t_cont->registerNs( pml => 'http://ufal.mff.cuni.cz/pdt/pml/' );
-    my @t_trees = $t_cont->findnodes('/pml:tdata/pml:trees/pml:LM');
-    my ($t_schema) = $t_cont->findnodes('/pml:tdata/pml:head/pml:schema');
-    $t_schema->setAttribute( 'href', 'tdata_mwe_schema.xml' );
-
-    # Modify the s-nodes to the correct form and merge them into t-trees
-    my @snodes = $s_cont->findnodes('/pml:sdata/pml:wsd/pml:st');
-  SNODE:
-    foreach my $snode (@snodes) {
-        my @tnode_rf = $s_cont->findnodes( './pml:t.rf', $snode );
-        map $_->unbindNode, @tnode_rf;
-        my $tnodes = $sdoc->createElement('tnode.rfs');
-        $tnodes = $snode->appendChild($tnodes);
-        map {
-            $_->setNodeName('LM');
-            my ($textchild) = $_->childNodes;
-            $textchild->replaceDataRegEx( 't#t', 't' );
-            $tnodes->appendChild($_);
-        } @tnode_rf;
-        my ($s_first_tnode) = map $_->toString,
-          $s_cont->findnodes( './pml:LM/text()', $tnodes );
-
-      TNODE: foreach my $troot (@t_trees) {
-            no warnings;
-            my @lmembers = $t_cont->findnodes( './/pml:LM', $troot );
-            my @tnode_ids = map $_->getAttribute('id'), @lmembers;
-            my ($match) = grep $_ eq $s_first_tnode, @tnode_ids;
-            my ( $mwes_exist, $mwes );
-            if ($match) {
-                if ( $t_cont->exists( './mwes', $troot ) ) {
-                    $mwes_exist = 1;
-                    ($mwes) = $t_cont->findnodes( './mwes', $troot );
-                }
-                else {
-                    $mwes = $tdoc->createElement('mwes');
-                    $troot->insertBefore( $mwes, $troot->firstChild );
-                }
-                my $snode_parent = $snode->parentNode;
-                $snode = $snode_parent->removeChild($snode);
-                $mwes->appendChild($snode);
-            }
-        }
+    my $tdoc = SDataMerge::transform($sdoc);
+    if ($use_stdout) {
+      $tdoc->toFH(\*STDOUT);
+    } else {
+      my $tmwe_filename = $tdoc->URI . ".mwe.gz";
+      $tdoc->setCompression('6');
+      $tdoc->toFile( $tmwe_filename, 1 );
     }
-    my $tmwe_filename = $t_filename . ".mwe.gz";
-    $tdoc->setCompression('6');
-    $tdoc->toFile( $tmwe_filename, 1 );
 }
