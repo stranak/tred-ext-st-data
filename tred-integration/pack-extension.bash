066b911c21d757f2ed465574a17357197e74b5ca
Pavel Stranak <pavel.stranak@gmail.com>
2010-04-28 17:19:23 +0200

    skript na sbaleni a odeslani tredi extenze do repozitare
    Signed-off-by: Pavel Stranak <pavel.stranak@gmail.com>

---
+++ tred-integration/pack-extension.bash
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+## Make tred extension with all macros.
+## Author: Jan Stepanek
+
+EXTNAME=pdt_t_st
+
+bindir=$(readlink -f ${0%/*})
+EXTDIR=/net/work/projects/tred/extensions
+WWW=/home/pajas/WWW/tred/extensions
+LOGO=icon.png
+
+cd "$bindir"/../tred-extension
+
+"$EXTDIR"/make $EXTNAME
+mkdir -p "$WWW"/$EXTNAME/icons
+cp $EXTNAME/icons/$LOGO  "$WWW"/$EXTNAME/icons
+chmod g+w /home/pajas/WWW/tred/extensions/$EXTNAME.zip
+rsync -avr "$WWW"/{index.html,$EXTNAME{,.zip}}
+ufal:/home/pajas/WWW/tred/extensions/
+rm -rf "$WWW"/$EXTNAME/icons/
